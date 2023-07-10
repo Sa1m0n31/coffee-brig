14a982fd3a898a75482dba204a7bf0dd678c814f
Szymon Burak <sajmon0031@gmail.com>
2023-07-10 11:06:50 +0200

    Start build desktop version of homepage

---
+++ main.js
@@ -0,0 +1,10 @@
+const productsSlider = new Siema({
+    selector: '.productsSlider',
+    duration: 400,
+    easing: 'ease-out',
+    perPage: 4,
+    startIndex: 0,
+    threshold: 20,
+    loop: true
+});
+
