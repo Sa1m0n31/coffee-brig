56b782d1be1c317f374c8e43b60e1b79c9bcf8d2
Szymon Burak <sajmon0031@gmail.com>
2023-07-14 23:19:26 +0200

    Add shop page, continue work on layout

--- storefront/theme-customisations/custom/custom.js
+++ storefront/theme-customisations/custom/custom.js
@@ -696,67 +696,77 @@ let historySlider, productsSlider, productsSliderIndicator;
 let mobileMenu, mobileMenuChildren;
 
 document.addEventListener('DOMContentLoaded', () => {
-    productsSlider = new Siema({
-        selector: '.productsSlider',
-        duration: 400,
-        easing: 'ease-out',
-        perPage: {
-            100: 1,
-            768: 2,
-            996: 3,
-            1200: 4
-        },
-        startIndex: 0,
-        threshold: 20,
-        loop: true,
-        draggable: false
-    });
+    try {
+        productsSlider = new Siema({
+            selector: '.productsSlider',
+            duration: 400,
+            easing: 'ease-out',
+            perPage: {
+                100: 1,
+                768: 2,
+                996: 3,
+                1200: 4
+            },
+            startIndex: 0,
+            threshold: 20,
+            loop: true,
+            draggable: false
+        });
+    }
+    catch(e) {
 
-    productsSliderIndicator = document.querySelector('.productsSliderLine__line__indicator');
-    const numberOfProducts = 16;
-
-    const getCurrentSlide = () => {
-        const currentSlide = productsSlider.currentSlide;
-
-        if(currentSlide >= 0) {
-            return currentSlide;
-        }
-        else {
-            return (numberOfProducts - 4) + (-1 * (-4 - currentSlide));
-        }
     }
 
-    const getProductsSliderIndicatorPosition = () => {
-        if(numberOfProducts) {
-            const currentSlide = getCurrentSlide();
-            const left = currentSlide / (numberOfProducts - 1);
-
-            return 75 * left;
-        }
-        else {
-            return '';
-        }
+    try {
+        historySlider = new Siema({
+            selector: '.historySlider',
+            duration: 400,
+            easing: 'ease-out',
+            perPage: {
+                100: 2,
+                768: 4,
+                996: 6,
+                1400: 8
+            },
+            startIndex: 0,
+            threshold: 20,
+            loop: false,
+            draggable: true
+        });
     }
+    catch(e) {
 
-    historySlider = new Siema({
-        selector: '.historySlider',
-        duration: 400,
-        easing: 'ease-out',
-        perPage: {
-            100: 2,
-            768: 4,
-            996: 6,
-            1400: 8
-        },
-        startIndex: 0,
-        threshold: 20,
-        loop: false,
-        draggable: true
-    });
+    }
 
     mobileMenu = document.querySelector('.mobileMenu');
     mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
+    productsSliderIndicator = document.querySelector('.productsSliderLine__line__indicator');
 });
+
+const numberOfProducts = 16;
+
+const getCurrentSlide = () => {
+    const currentSlide = productsSlider.currentSlide;
+
+    if(currentSlide >= 0) {
+        return currentSlide;
+    }
+    else {
+        return (numberOfProducts - 4) + (-1 * (-4 - currentSlide));
+    }
+}
+
+const getProductsSliderIndicatorPosition = () => {
+    if(numberOfProducts) {
+        const currentSlide = getCurrentSlide();
+        const left = currentSlide / (numberOfProducts - 1);
+
+        return 75 * left;
+    }
+    else {
+        return '';
+    }
+}
 
 const changeIndicatorPosition = () => {
     productsSliderIndicator.style.left = `${getProductsSliderIndicatorPosition()}%`;
--- storefront/theme-customisations/custom/templates/archive-product.php
+++ storefront/theme-customisations/custom/templates/archive-product.php
@@ -4,233 +4,72 @@ $home_url = 'https://metahuman.pl';
 ?>
 
 <div class="row categories">
-    <div class="col-sm-4 col-md-3 col-lg-2 list-categories">
-        <nav class="navbar navbar-expand-lg navbar-categories" aria-label="Offcanvas navbar large">
-            <div class="container-fluid noMarginLeftOnMobile">
-                <button class="btn categories navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNavbar2"
-                        aria-controls="offcanvasNavbar2">
-                    <p>Kategorie<i class="bi bi-caret-down-fill"></i></p>
-                </button>
-                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar2"
-                     aria-labelledby="offcanvasNavbar2Label">
-                    <div class="offcanvas-header">
-                        <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas"
-                                aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body categoriesWrapper noscroll">
-                        <ul class="list-group categories-ul">
-                            <li class="list-group-item">Kategorie</li>
-                        </ul>
-                        <ul class="list-group list-group-flush">
+    <div class="row new-stuff-container">
+        <h3 class="sectionHeader">Sklep</h3>
 
+        <div class="productPreviewContainer">
+            <?php
+            $sort = 0;
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+            $loop = new WP_Query( array(
+                'post_type' => 'product',
+                'post_status' => 'publish',
+                'orderby' => 'publish_date',
+                'order' => 'asc',
+                'paged' => $paged
+            ));
 
-                            <?php
+            ?>
 
-                            $taxonomy     = 'product_cat';
-                            $orderby      = 'name';
-                            $show_count   = 0;      // 1 for yes, 0 for no
-                            $pad_counts   = 0;      // 1 for yes, 0 for no
-                            $hierarchical = 1;      // 1 for yes, 0 for no
-                            $title        = '';
-                            $empty        = 0;
+            <?php
 
-                            $args = array(
-                                'taxonomy'     => $taxonomy,
-                                'orderby'      => $orderby,
-                                'show_count'   => $show_count,
-                                'pad_counts'   => $pad_counts,
-                                'hierarchical' => $hierarchical,
-                                'title_li'     => $title,
-                                'hide_empty'   => $empty
-                            );
-                            $all_categories = get_categories( $args );
-                            foreach ($all_categories as $cat) {
-                                if($cat->category_parent == 0) {
-                                    $category_id = $cat->term_id;
+            if($loop->have_posts()) {
+                while($loop->have_posts()) {
+                    $loop->the_post();
+                    global $product;
+                    $salePrice = $product->get_sale_price();
+                    $regularPrice = $product->get_regular_price();
 
-                                    $args2 = array(
-                                        'taxonomy'     => $taxonomy,
-                                        'child_of'     => 0,
-                                        'parent'       => $category_id,
-                                        'orderby'      => $orderby,
-                                        'show_count'   => $show_count,
-                                        'pad_counts'   => $pad_counts,
-                                        'hierarchical' => $hierarchical,
-                                        'title_li'     => $title,
-                                        'hide_empty'   => $empty
-                                    );
+                    #1 Get product variations
+                    $product_variations = $product->get_available_variations();
 
-                                    $sub_cats = get_categories( $args2 );
+                    #2 Get one variation id of a product
+                    $variation_product_id = $product_variations[0]['variation_id'];
 
-                                    if(0) {
-                                        ?>
+                    #3 Create the product object
+                    $variation_product = new WC_Product_Variation( $variation_product_id );
 
-                                        <?php
-                                    }
-                                    else {
-                                        echo '<li class="list-group-item"><a href="' . get_term_link($cat->slug, 'product_cat') . '">' . $cat->name . '</a></li>';
-                                    }
-                                }
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </div>
+                    $regularPrice = $variation_product->regular_price;
+                    $salePrice = $variation_product ->sale_price;
+                    $discountPercent = 0;
 
-    <div class="col-sm-8 col-md-9 col-lg-10">
-        <div class="row new-stuff-container">
-            <div class="col-sm-12 category-title">
-                <section class="text-center">
-                    <h3 class="sectionHeader"><b>Sklep</b></h3>
-                </section>
-            </div>
-
-            <div class="productPreviewContainer">
-                <?php
-                $sort = 0;
-                $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                if($_GET['sort'] == 'new') {
-                    $sort = 0;
-                    $loop = new WP_Query( array(
-                        'post_type' => 'product',
-                        'post_status' => 'publish',
-                        'orderby' => 'publish_date',
-                        'order' => 'asc',
-                        'paged' => $paged
-                    ));
-                }
-                else if($_GET['sort'] == 'price-asc') {
-                    $sort = 1;
-                    $loop = new WP_Query( array(
-                        'post_type' => 'product',
-                        'post_status' => 'publish',
-                        'orderby'   => 'meta_value_num',
-                        'meta_key'  => '_price',
-                        'order' => 'asc',
-                        'paged' => $paged
-                    ));
-                }
-                else if($_GET['sort'] == 'price-desc') {
-                    $sort = 2;
-                    $loop = new WP_Query( array(
-                        'post_type' => 'product',
-                        'post_status' => 'publish',
-                        'orderby'   => 'meta_value_num',
-                        'meta_key'  => '_price',
-                        'order' => 'desc',
-                        'paged' => $paged
-                    ));
-                }
-                else if($_GET['sort'] == 'by-name') {
-                    $sort = 3;
-                    $loop = new WP_Query( array(
-                        'post_type' => 'product',
-                        'post_status' => 'publish',
-                        'orderby'   => 'title',
-                        'order' => 'asc',
-                        'paged' => $paged
-                    ));
-                }
-                else {
-                    $loop = new WP_Query( array(
-                        'post_type' => 'product',
-                        'post_status' => 'publish',
-                        'paged' => $paged
-                    ));
-                }
-
-                ?>
-
-                <?php
-
-                if($loop->have_posts()) {
-                    while($loop->have_posts()) {
-                        $loop->the_post();
-                        global $product;
-                        $salePrice = $product->get_sale_price();
-                        $regularPrice = $product->get_regular_price();
-
-                        #1 Get product variations
-                        $product_variations = $product->get_available_variations();
-
-                        #2 Get one variation id of a product
-                        $variation_product_id = $product_variations[0]['variation_id'];
-
-                        #3 Create the product object
-                        $variation_product = new WC_Product_Variation( $variation_product_id );
-
-                        $regularPrice = $variation_product->regular_price;
-                        $salePrice = $variation_product ->sale_price;
-                        $discountPercent = 0;
-
-                        if($regularPrice && $salePrice) {
-                            $discountPercent = round($salePrice / $regularPrice * 100);
-                        }
-
-                        ?>
-                        <div class="<?php if($salePrice) echo "productPreview discount-col"; else echo "productPreview"; ?>">
-                            <a href="<?php echo get_permalink( $product->get_id() ); ?>"">
-                            <?php
-                            if($product->is_on_sale()) {
-                                ?>
-                                <div class="discount">
-                                        <span>
-                                            -<?php echo $discountPercent; ?> %
-                                        </span>
-                                </div>
-                                <?php
-                            }
-                            ?>
-
-                            <img class="img-fluid" src="<?php echo wp_get_attachment_url( $product->get_image_id() ); ?>"
-                                 alt="picture1">
-                            <img class="img-fluid img--second" src="<?php echo get_field('drugie_zdjecie'); ?>"
-                                 alt="picture1">
-
-                            <?php
-                                echo do_shortcode('[favorite_button]');
-                            ?>
-
-                            <ul class="list-group">
-                                <li class="list-group-item">
-                                    <b>
-                                        <?php echo the_title(); ?>
-                                    </b>
-                                </li>
-                                <li class="list-group-item">
-                                    <?php echo get_field('zajawka'); ?>
-                                </li>
-                            </ul>
-                            <ul class="list-inline discount-price">
-                                <?php
-                                if($product->is_on_sale()) {
-                                    ?>
-                                    <li class="list-inline-item oldPrice"><?php echo$regularPrice; ?> PLN</li>
-                                    <li class="list-inline-item"><?php echo $salePrice; ?> PLN</li>
-                                    <?php
-                                }
-                                else {
-                                    ?>
-                                    <li class="list-inline-item"><?php echo $regularPrice; ?> PLN</li>
-                                    <?php
-                                }
-                                ?>
-                            </ul>
-                            </a>
-                        </div>
-                        <?php
+                    if($regularPrice && $salePrice) {
+                        $discountPercent = round($salePrice / $regularPrice * 100);
                     }
-                    ?>
 
+                    ?>
+                    <a class="productsSlider__item" href="<?php echo get_permalink( $product->get_id() ); ?>">
+                            <span class="productsSlider__item__top">
+                                <?php echo get_field('napis_na_gorze'); ?>
+                            </span>
+                        <figure class="productsSlider__item__image center">
+                            <img class="img" src="<?php echo wp_get_attachment_url( $product->get_image_id() ); ?>" alt="kawa" />
+                        </figure>
+                        <h4 class="productsSlider__item__title">
+                            <?php echo the_title(); ?>
+                        </h4>
+                        <h5 class="productsSlider__item__price">
+                            od <?php echo $regularPrice; ?> zł
+                        </h5>
+                    </a>
                     <?php
-                    wp_reset_postdata();
                 }
                 ?>
-            </div>
+
+                <?php
+                wp_reset_postdata();
+            }
+            ?>
         </div>
     </div>
 </div>
